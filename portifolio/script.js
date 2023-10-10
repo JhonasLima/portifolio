3790713cecfa9fa9d7260875d4e7da53c64b04ee
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2023-10-09 22:07:10 -0300

    atualizei

--- portifolio/script.js
+++ portifolio/script.js
@@ -1,7 +1,12 @@
 function clickMenu(){
+    var icon = document.querySelector("#icon-menu")
     if (menu.style.display == 'flex'){
         menu.style.display = 'none'
+        icon.innerHTML = "Menu"
+        icon.style.border = 'none'
     } else {
         menu.style.display = 'flex'
+        icon.innerHTML = "Close"
+        icon.style.border = '2px solid rgba(255, 255, 255, 0.575)'
     }
 }
