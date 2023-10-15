6b4b0f5be67362c77faf152b0fb6e6882ab1e878
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2023-10-15 11:06:34 -0300

    atualização

--- portifolio/script.js
+++ portifolio/script.js
@@ -11,11 +11,11 @@ function clickMenu(){
 }
 
 function clickImg() {
-    if (imgLogo.style.width == '90px'){
+    if (imgLogo.style.width == '85px'){
         imgLogo.style.width = '150px'
         /*imgLogo.style.position = 'absolute'*/
     } else {
-        imgLogo.style.width = '90px'
+        imgLogo.style.width = '85px'
         imgLogo.style.position = 'relative'
     }
 }
