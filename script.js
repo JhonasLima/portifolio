e4909d22c12272e5561f930182d259a86675681c
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2023-11-03 14:31:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function clickCard() {
             containerCard.style.flexDirection = 'column'
 
             setTimeout(function () {
-
+                
                 elemento.style.opacity = 1
 
             }, 500)
@@ -49,11 +49,14 @@ function clickCard() {
         } else {
             elemento.style.display = 'none'
             containerCard.style.flexDirection = 'row'
-            setTimeout(function () {
-                elemento.style.opacity = 0
-                document.getElementById("conhecimentos").focus()
-            }, 500)
             
+            document.getElementById('conhecimentos').scrollIntoView({behavior: "smooth", block: "center"})
+
+            setTimeout(function () {
+                
+                elemento.style.opacity = 0
+
+            }, 500)
         }
     })
 }
