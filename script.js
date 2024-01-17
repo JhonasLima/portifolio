d3bcf83797f6e1e118bdfa9dbd82758abafb2e4d
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2024-01-16 23:37:39 -0300

    atualização gif carregamento

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = function () {
         var loading = document.getElementById('loading')
 
         loading.parentNode.removeChild(loading)
-    }, 1)
+    }, 1500)
 }
 
 /*----------------------------Idade------------------------*/
