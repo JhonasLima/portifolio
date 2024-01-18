8fec5fd77bdac587ff37008f333022794f53f75a
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2024-01-17 23:30:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = function () {
         var loading = document.getElementById('loading')
 
         loading.parentNode.removeChild(loading)
-    }, 1300)
+    }, 1500)
 }
 
 /*----------------------------Idade------------------------*/
